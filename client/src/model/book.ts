c0b9cf73692740c39349a4b7d6be24252a826aa7
Ozimek <albert.ozimek@gmail.com>
2019-01-09 15:03:20 +0100

    minor

--- client/src/model/book.ts
+++ client/src/model/book.ts
@@ -1,17 +1,9 @@
 export class Book {
-    private id: string;
-    private title: string;
-    private author: string;
-    private publicationDate;
-    private publisher;
-    private price;
-
-    constructor(id: string, title: string, author: string, publicationDate, publisher, price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publicationDate = publicationDate;
-        this.publisher = publisher;
-        this.price = price;
+    constructor( private id: string,
+    private title: string,
+    private author: string,
+    private publicationDate,
+    private publisher,
+    private price) {
     }
 }
