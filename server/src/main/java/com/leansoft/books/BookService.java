ed40b524da03dcfc5d0c5d3c39d386381ac7f655
Ozimek <albert.ozimek@gmail.com>
2019-01-09 12:34:11 +0100

    POST request for creation

--- server/src/main/java/com/leansoft/books/BookService.java
+++ server/src/main/java/com/leansoft/books/BookService.java
@@ -1,5 +1,6 @@
 package com.leansoft.books;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
 	
-	private List<Book>  books = Arrays.asList(
+	private List<Book>  books = new ArrayList<>(Arrays.asList(
 			new Book("111", "Eine kurze Geschichte der Menscheit", "Yuval Noah Harari", "2016", "DVA", 36),
 			new Book("222", "Homo Deus", "Yuval Noah Harari", "2017", "CHBECK", 37),
-			new Book("333", "21 Lektionen", "Yuval Noah Harari", "2017", "CHBECK", 37));
+			new Book("333", "21 Lektionen", "Yuval Noah Harari", "2017", "CHBECK", 37)));
 	
 	public List<Book> getAllBooks() {
 		return this.books;
@@ -19,4 +20,8 @@ public class BookService {
 	public Book getBook(String id) {
 		return this.books.stream().filter( b -> b.getId().equals(id)).findFirst().get();
 	}
+
+    public void addBook(Book book) {
+	    this.books.add(book);
+    }
 }
--- server/src/main/java/com/leansoft/books/BooksController.java
+++ server/src/main/java/com/leansoft/books/BooksController.java
@@ -2,9 +2,7 @@ package com.leansoft.books;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BooksController {
@@ -23,4 +21,8 @@ public class BooksController {
 		return this.bookService.getBook(id);
 	}
 
+	@RequestMapping(method = RequestMethod.POST, value = "/books")
+	public void addBook(@RequestBody Book book) {
+       this.bookService.addBook(book);
+    }
 }
